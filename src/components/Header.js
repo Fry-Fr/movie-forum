0a4cde48157636e3462228527138b20e93376034
Fry-Fr <68477278+Fry-Fr@users.noreply.github.com>
2022-01-01 01:25:06 -0500

    basic movie forum with header

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import Nav from "./Nav";
+
+function Header() {
+    return (
+        <header>
+            <h2>My Movie Watch List</h2>
+            <Nav/>
+        </header>
+    )
+}
+
+export default Header;
