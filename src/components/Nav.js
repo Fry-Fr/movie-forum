514019f268555dec64bf676467c60f7e9f9775d8
Fry-Fr <68477278+Fry-Fr@users.noreply.github.com>
2022-01-01 01:51:29 -0500

    basic Header, Nav scaffold

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,9 +3,9 @@ import { Link } from 'react-router-dom';
 function Nav() {
     return (
         <nav className="nav-container">
-            <Link to="">My Movies</Link>
-            <Link to="">worth the time</Link>
-            <Link to="">waste of time</Link>
+            <Link to="/">My Movies</Link>
+            <Link to="/good">worth the time</Link>
+            <Link to="/not_good">waste of time</Link>
         </nav>
     )
 }
