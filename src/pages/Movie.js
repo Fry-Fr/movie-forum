cbc1fcde6ca8d5568a9657a0af4cf960dcb9c3b8
Fry-Fr <68477278+Fry-Fr@users.noreply.github.com>
2022-01-01 23:36:44 -0500

    basic scaffold for Movie page

--- src/pages/Movie.js
+++ src/pages/Movie.js
@@ -5,16 +5,17 @@ function Movie({ setCompon, apiMovies }) {
     const [movie, setMovie] = useState();
     const { id } = useParams();
     useEffect(() => {
-        setCompon(`<Movie ${id} />`);
         const _movie = apiMovies.filter(movie => movie.id === Number(id))
         setMovie(_movie[0])
-    },[setCompon, apiMovies, id])
+        if (movie) {
+            setCompon(`<${movie.title}/>`)
+        }
+    },[setCompon, apiMovies, id, movie])
 
     return (
         <div className="movie-container">
             {!movie ? undefined : 
             <>
-            <div>{movie.title}</div>
             <div>{movie.rating}</div>
             <div>{movie.description}</div>
             </>
