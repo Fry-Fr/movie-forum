b06d2e3c4536b8e98531363a8a3c8a84abcb37ef
Fry-Fr <68477278+Fry-Fr@users.noreply.github.com>
2022-01-03 03:36:26 -0500

    axios req returns movie object

--- src/pages/SearchResults.js
+++ src/pages/SearchResults.js
@@ -1,12 +1,29 @@
+import axios from "axios";
+
 function SearchResults({ results }) {
+    const handleAddMovieTodb = (e) => {
+        e.stopPropagation();
+        console.log('imdb_id', e.target.id, e.target)
+        const options = {
+            method: 'GET',
+            url: `https://data-imdb1.p.rapidapi.com/movie/id/${e.target.id}/`,
+            headers: {
+              'x-rapidapi-host': 'data-imdb1.p.rapidapi.com',
+              'x-rapidapi-key': '5a7004bf33mshe25977a0a22602cp172ef1jsn8587be47b445'
+            }
+          };
+        axios.request(options).then(response => {
+            console.log(response.data)
+        }).catch(error => console.log(error))
+    }
+
     return (
         <>
         <div className="search-result">
             {!results ? undefined : results.map((res, i) => {
                 return (
-                    <div key={i} className="search-result-card">
-                        <span>{res.imdb_id}</span>
-                        <span>{res.title}</span>
+                    <div key={i} id={res.imdb_id} className="search-result-card" onClick={handleAddMovieTodb}>
+                        <span id={res.imdb_id}>{res.title}</span>
                     </div>
                 )
             })}
