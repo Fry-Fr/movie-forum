e956c326ae66c9c74979440a36e2962fb0024a7a
Fry-Fr <68477278+Fry-Fr@users.noreply.github.com>
2022-01-04 23:12:08 -0500

    function for handle is_good rating

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,6 +1,16 @@
 import { Link } from "react-router-dom";
 
 function MovieCard({ movies }) {
+    const handleIsGoodRatings = (bool) => {
+        if (bool === null) {
+            return undefined
+        }else if (bool === true) {
+            return '👍'
+        }else {
+            return '👎'
+        }
+    }
+
     return (
        <>
        {!movies ? undefined : movies.map((movie, i) => {
@@ -8,7 +18,7 @@ function MovieCard({ movies }) {
             <Link to={`/movie/${movie.movie_id}`} key={i} className="movie-card-link">
                 <div className="movie-card">
                     <span>{movie.title}</span>
-                    <span>{movie.is_good ? '👍' : '👎'}</span>
+                    <span>{handleIsGoodRatings(movie.is_good)}</span>
                     <span>{movie.rating}</span>
                     <span>{movie.description}</span>
                 </div>
